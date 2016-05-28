2964ce580b2246dc836864050f7a0fd082ff8f4e
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2016-05-28 21:58:19 +0400

    OOPify

--- IzzyGeo.py
+++ IzzyGeo.py
@@ -4,17 +4,27 @@
 
 # Description:		This script eases creation of Georgian terms on http://en.wiktionary.org
 # Author:		Dixtosa
-# Language:		Python 2
+# Language:		Python 3
 # Date created:		18 Feb 2012 (initial version)
 
 import re, sys
+
 
 letters="abgdevzTiklmnopJrstufqRySCcZwWxjh"
 vowel = "aeiou"
 consonants = "".join (set(letters) - set(vowel))
 
-ENTRY = """\
-==Georgian==
+
+############# unofficial transliteration ################
+def LAT_2_GEO(text):
+	res = ""
+	for i in text:
+		if i in letters: res+=chr(4304+letters.index(i))
+		else: res+=i
+	return res
+	
+
+ENTRY = """==Georgian==
 
 ===Pronunciation===
 * {{ka-IPA}}
@@ -25,6 +35,16 @@ ENTRY = """\
 
 # %s
 %s"""
+
+def Generate_Content(
+        Header,
+        Template,
+        plural,
+        definition,
+        DECLENSION
+        ):
+	plural = "|" + plural if plural != "" else ""
+	return ENTRY % (Header, Template, plural, definition, DECLENSION)
 
 Headers = ["Noun" ,"Adjective" ,"Verbal noun" ,"Adverb" ,"Interjection" ,"Verb" ,"Pronoun" ,"Proper noun" ,"Particle" ,"Phrase" ,"Postposition"]
 
@@ -46,95 +66,63 @@ Header2Template = {
 # noun and verb forms
 Forms = ["Noun", "Verb"]
 
-############# unnoficial transliteration ################
-def LAT_2_GEO(text):
-	res=unicode("")
-	for i in text:
-		if i in letters:
-			res+=unichr(4304+letters.index(i))
-		else:
-			res+=i
-	return res.encode("utf-8")
 
-############# end of unofficial transliteration ################
+def GetDeclension(entry):
+	arg1, arg2 = "", ""
+	if not entry.hasPlural():
+		arg1 = "|-"
+	elif not entry.hasPredictablePlural():
+		arg1 = "|" + plural[:-3]
+	return "\n====Inflection====\n{{ka-infl-noun%s%s}}" % (LAT_2_GEO(arg1), LAT_2_GEO(arg2))
 
-
-def GetDeclension(STEM_TYPE, Plural, Stem, PoSi):
-	DECLENSION="\n\n====Inflection====\n{{ka-infl-noun}}"
-	return DECLENSION
-
-def GetPlural(Word):
-	ans = raw_input("""
+def AskPlural(term):
+	ans = input("""
 Does it have plural? 
-	+ YES, is it different from: """+Word[:-1]+ """ebi?
-		- NO  >> press enter
-		- YES >> type
-	+ NO >> type 'no':\n""")
+	+ YES, is it different from: """ + term[:-1] + """ebi?
+		- NO  : press enter
+		
+		- YES : type the exceptional plural
+	- NO : type 'no'\n""")
+	return "-" if ans == "ara" or ans == "no" else ans
 	
-	if ans=="ara" or ans=="no": return "-";
-	elif ans!="": return ans
 	
-	if Word[-1] in "ia": return Word[:-1]+"ebi"
-	if Word[-1] in "eou": return Word+"ebi"
-	
-	return Plural
-
-def GetStem (Word):
-	if Word[-1] in "iae": return Word[:-1]
-	elif Word[-1] in "ou": return Word
-
-def GetStemType (Word):
-	if (Word[:-1][-1] in consonants) and (Word[-1]=="i"): return "TanxmovanfuZiani"
-	if Word[-1]=="o" or Word[-1]=="u": return "oufuZiani"
-	return Word[-1] + "fuZiani"
 
 # I admit this function is overcomplicated w/o any reason xD
 def getPartOfSpeech():
-	print "Choose part of speech:"
+	print("Choose part of speech:")
 
 	for i, name in enumerate(Headers):
-		print "%02d) %s" % (i+1, name + ("*" if name in Forms else ""))
+		print("%02d) %s" % (i+1, name + ("*" if name in Forms else "")))
 	
-	ans = re.match(r"(\d\d?)(\*?)$", raw_input())
+	ans = re.match(r"(\d\d?)(\*?)$", input())
 	if ans:
 		d, ast = ans.groups()
 		d = int(d) - 1
 		if d < len(Headers):
 			return Headers[d] + ast
 	
-	print "Doh come on it was easy. Try again..."
+	print("Doh come on it was easy. Try again...")
 	sys.exit(0)
 
 def open_browser(Word):
-	import webbrowser, urllib
+	import webbrowser, urllib.request, urllib.parse, urllib.error
 	
 	URL="http://en.wiktionary.org/w/index.php?title=%s&action=edit"
 
-	webbrowser.open_new_tab(URL%(urllib.quote_plus(LAT_2_GEO(Word))))
+	webbrowser.open_new_tab(URL%(urllib.parse.quote_plus(LAT_2_GEO(Word))))
 
 
-def Copy_To_Clip(content):
-	import win32clipboard
-	win32clipboard.OpenClipboard(0)
-	win32clipboard.EmptyClipboard()
-	win32clipboard.SetClipboardData(win32clipboard.CF_UNICODETEXT, content.decode("utf-8"))
-	win32clipboard.CloseClipboard
+def Copy_To_Clipboard(content):
+	from subprocess import Popen, PIPE, STDOUT
+	subp = Popen(["clip"], stdout=PIPE,stdin=PIPE)
+	subp.communicate(input = bytearray(content, "UTF-16")[2:]); #rm BOM?
 
-	
-def Generate_Content(
-        Header,
-        Template,
-        Plural,
-        definition,
-        DECLENSION
-        ):
-	return ENTRY % (Header, Template, Plural, definition, DECLENSION)
 
 def Getlexeme():
-	return raw_input ("lexeme: ")
+	return input ("lexeme: ")
 
 def GenerateEnglishTranslation():
-	Eng_trans = raw_input ("English infinitive: ")
+	Eng_trans = input ("English infinitive: ")
 	conj = "\n\n====Conjugation====\n{{ka-conj-transitive-ablaut\n|stem =\n|ablaut =\n|preverb =\n|versioner=\n|SNM=\n}}"
 	return "She/he/it [[" + Eng_trans+"]]s, is [["+Eng_trans+"]]ing" + conj
 
@@ -142,15 +130,15 @@ def GenerateEnglishTranslation():
 
 def Generate_Verb_Definition():
 	lexeme=LAT_2_GEO(Getlexeme())
-	ans = int(raw_input("1) 1\n2) 2\n3) 3\n"))
-	plurality=int(raw_input("1) Singular\n2) Plural:\n"))
+	ans = int(input("1) 1\n2) 2\n3) 3\n"))
+	plurality=int(input("1) Singular\n2) Plural:\n"))
 	#persons = ["First", "Second", "Third"]
 	plurality_list = ["sg", "pl"]
 	#time = ["Present indicative", "Imperfect", "Present subjunctive", "Future indicative", "Conditional", "Future subjunctive",
         #        "Aorist indicative", "Optative",
         #        "Perfect", "Pluperfect", "Perfect subjunctive"]
 	text = "{{ka-verb-form-of|"+str(ans)+"|"+plurality_list[plurality-1]
-	ans_time = int(raw_input("""
+	ans_time = int(input("""
 1) awmyo			ras shvreba?	[axlandeli]
 2) uwyveteli			ras shvreboda?
 3) awmyos kavSirebiTi		ras shvrebodes?
@@ -183,39 +171,99 @@ def getDefinition(partOfSpeech):
 	elif partOfSpeech == "Verb":
 		return GenerateEnglishTranslation()
 	elif partOfSpeech == "Verbal noun":
-		return "{{ka-verbal for|" + LAT_2_GEO(raw_input ("Lemma: ")) + "}}"
+		return "{{ka-verbal for|" + LAT_2_GEO(input ("Lemma: ")) + "}}"
 	else:
 		# ugly but cool XD
-		ans = raw_input("In English (separate with , and ;):").replace(";", "]]\n# [[")
+		ans = input("In English (separate with , and ;):").replace(";", "]]\n# [[")
 		return "[[" + ans.replace(",", "]], [[") + "]]"
 
+class GenericWord(object):
+	def __init__(self, term):
+		self.term = term
+		self.partOfSpeech = None
+		self.definitionLine = None
+	def run(self):
+		pass
+	def generateContent(self):
+		if self.plural != "":
+			self.plural = "|" + self.plural
+		
+		return self.entryTemplate % (self.partOfSpeech, self.headwordTemplate, self.plural, self.definitionLine, self.declension)
+
+class Noun:
+	def __init__(self, term):
+		self.term = term
+	def setPartOfSpeech(self, partOfSpeech):
+		self.partOfSpeech = partOfSpeech
+	def run(self):
+		self.definition = getDefinition(self.partOfSpeech)
+		self.headwordTemplate = getHeadwordTemplate(self.partOfSpeech)
+		
+		if self.partOfSpeech in ["Noun", "Proper noun"]:
+			self.plural = self.getPlural()
+			self.declension = "" or GetDeclension(self)
+	def generateContent(self):
+		return Generate_Content(
+			self.partOfSpeech.replace("*", ""),
+			self.headwordTemplate,
+			LAT_2_GEO(self.plural),
+			self.definition,
+			self.declension
+		)
+	def hasPlural(self):
+		return self.plural != "-"
+	def getPlural(self):
+		ans = AskPlural(self.term)
+		return self.guessPlural() if ans == "" else ans
+	def guessPlural(self):
+		if self.term[-1] in "ia":
+			return self.term[:-1] + "ebi"
+		if self.term[-1] in "eou":
+			return self.term + "ebi"
+	def hasPredictablePlural(self):
+		return self.guessPlural() == self.plural
+		
+class Adjective:
+	pass
+class VerbalNoun:
+	pass
+class Verb:
+	pass
+class Pronoun:
+	pass
+class Wordform:
+	pass
+
+header2class = {
+"Noun"		:	Noun,
+"Adjective"	:	Adjective,
+"Verbal	noun"	:	VerbalNoun,
+"Adverb"	:	GenericWord,
+"Interjection"	:	GenericWord,
+"Verb"		:	Verb,
+"Pronoun"	:	Pronoun,
+"Proper	noun"	:	Noun,
+"Particle"	:	GenericWord,
+"Phrase"	:	GenericWord,
+"Postposition"	:	GenericWord,
+
+"Noun*"		:	Wordform,
+"Verb*"		:	Wordform
+}
+
 def Main():
-	plural = ""
-	declension = ""
-	definition = ""
-	
 	partOfSpeech = getPartOfSpeech()
-	headwordTemplate = getHeadwordTemplate(partOfSpeech)
+	posClass = header2class[partOfSpeech]
 	
-	word = raw_input("Input word: ") #Input in Latin script, but use unnoficial romanization; see http://en.wikipedia.org/wiki/Romanization_of_Georgian
-	
-	if partOfSpeech in ["Noun", "Proper noun"]:
-		plural = "|" + GetPlural(word)
-		declension = "" or GetDeclension(GetStemType (word), plural, GetStem (word), partOfSpeech)
-	
-	definition = getDefinition(partOfSpeech)
-	
-	
-	content = Generate_Content(
-	        partOfSpeech.replace("*", ""),
-	        headwordTemplate,
-	        LAT_2_GEO(plural),
-	        definition,
-	        declension
-	)
-	
-	#print content
-	Copy_To_Clip(content)
-	open_browser(word)
+	term = input("Input the term: ") #Input in the Latin script, but use unofficial romanization; see http://en.wikipedia.org/wiki/Romanization_of_Georgian
+
+	entry = posClass(term)
+	entry.setPartOfSpeech(partOfSpeech)
+	entry.run()
+	content = entry.generateContent()
+	#print (content)
+	Copy_To_Clipboard(content)
+	open_browser(term)
+
 
 Main()
