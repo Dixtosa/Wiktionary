85357014b5f6f03071973f67f70a16e2c6b867be
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2016-10-25 00:29:26 +0400

    Update expandable.js
    started line offset stuff but meh

--- User Scripts/expandable.js
+++ User Scripts/expandable.js
@@ -1,52 +1,66 @@
-function expandabledotjs_changeWikitext(wikitext, number, text)
+// Description:   can make basic lists expandable without reloading a page
+//				  can be tested here [[User:Dixtosa/Georgian protologisms]]
+// Problems/Todo: see below "BAD_CODE"
+// can enjoy mw.api.parse and +- character offset
+
+let expandable = {};
+expandable.changeWikitext = function(wikitext, number, text)
 {
-	var replaceCounter = 0;
-	var prefix = "* [["
-	var suffix = "]]";
+	let replaceCounter = 0;
+	let prefix = "* [[";
+	let suffix = "]]";
+	let newlines = "\n";
 	wikitext = wikitext.replace(/{{expandable(.*?)}}/g,
-								function(match, capture)
-								{
-									if (replaceCounter++ == number)
-									{
-										capture = capture.split("|");
-										if (capture.length == 2){
-											prefix = capture[1];
-											suffix = "";
-										}
-										else if (capture.length == 3) {
-											prefix = capture[1];
-											suffix = capture[2];
-										}
-										prefix = decodeURIComponent(prefix);
-										suffix = decodeURIComponent(suffix);
-										return match + "\n" + prefix + text + suffix;
-									}
-									else return match;
-								}
-				);
+		function(match, capture) {
+			if (replaceCounter++ == number) {
+				capture = capture.split("|");
+				if (capture.length == 2) {
+					[prefix, suffix] = [capture[1], ""];
+				}
+				else if (capture.length == 3) {
+					[prefix, suffix] = [capture[1], capture[2]];
+				}
+				else if (capture.length > 3) {
+					[prefix, suffix] = [capture[1], capture[2]];
+
+					let offsetY = capture[3].match(/y=(\d+)/);
+					if (offsetY !== null) {
+						newlines += "\n".repeat(parseInt(offsetY[1]));
+					}
+				}
+				[prefix, suffix] = $.map([prefix, suffix], decodeURIComponent);
+				
+				return match + newlines + prefix + text + suffix;
+			}
+			else return match;
+		}
+	);
 	return wikitext;
-}
+};
 
 function insertStuff(number)
 {
-	var text = $("#expandabledotjs" + number).val();
-	var editor = Editor();
-	editor.addEdit({
- 
-			edit: function(wikitext){ return expandabledotjs_changeWikitext(wikitext, number, text); },
- 
-			redo: function(){ $("#expandabledotjs"+number).next().after("<ul><li>" + text + "</li></ul>"); },
- 
-			undo: function(){ $("#expandabledotjs"+number).next().next().remove(); },
- 
-			summary: "[[User:Dixtosa/expandable.js|expanded]]; "
-		});
+	let $expandable = $("#expandabledotjs" + number);
+	let text = $expandable.val();
+	new Editor().addEdit({
+		edit: wikitext => expandable.changeWikitext(wikitext, number, text),
+
+		redo: () => { $expandable.next().after("<ul><li>" + text + "</li></ul>"); },
+
+		undo: () => { $expandable.next().next().remove(); },
+
+		summary: "[[User:Dixtosa/expandable.js|expanded]]; "
+	});
 }
 
-var eachCounter = -1;
-$(".expandabledotjs").replaceWith(
-	function()
-	{
-		eachCounter++;
-		return "<form><input type = 'text' id = 'expandabledotjs" + eachCounter + "' /><input type='submit' value='+' onclick='javascript:insertStuff("+eachCounter+"); return false;'></form>";
-	});
+let eachCounter = -1;
+$(".expandabledotjs").replaceWith(() => {
+	eachCounter++;
+	let textInput = $("<input type = 'text'/>").attr("id", "expandabledotjs" + eachCounter); //BAD_CODE
+	let submit = $(`<input type='submit' value='+' onclick='javascript:insertStuff(${eachCounter}); return false;'>`);
+	return $("<form></form>").css("display", "inline").append(textInput).append(submit);
+});
+
+$(".expandabledotjs_affix").css("display", "inline").each(function() {
+	$(this).text(decodeURIComponent($(this).text()));
+});
