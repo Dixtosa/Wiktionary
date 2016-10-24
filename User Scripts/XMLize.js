487ccbdd650dbe173d562ba40fbb391dd0e74204
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2016-10-24 23:25:42 +0400

    Update XMLize.js

--- User Scripts/XMLize.js
+++ User Scripts/XMLize.js
@@ -1,101 +1,175 @@
 // Author : Dixtosa
-// Known users of this script: User:Dixtosa/minorEdits.js
- 
-function doNotInterpertSpecialCharacters(wikitext)
+// Known users of this script: [[User:Dixtosa/minorEdits.js]]
+
+//Lexemes (tags) are: section, category, splitter, interwikis
+
+if (!window.WikiXmlLoaded)
 {
-	//making sure some of the <data> will not be interpreted
-	wikitext = wikitext.replace (new RegExp("&", "g"), "&amp;"); //for entities
-	wikitext = wikitext.replace (new RegExp("<", "g"), "&lt;"); //for comments
-	return wikitext;
-}
-function XMLize(wikitext)
-{
-	wikitext = doNotInterpertSpecialCharacters(wikitext);
- 
-	var wikilist = wikitext.split("\n");
-	var oldLevel = 1;
-	var wikixml = "<xml><data>";
-	for (var i = 0, line = wikilist[0]; i < wikilist.length; i++, line = wikilist[i])
+	window.WikiXmlLoaded = true;
+	function doNotInterpertSpecialCharacters(wikitext)
 	{
-		if (line[line.length - 1] == line[0] && line[0] == '=')
+		//making sure some of the <data> will not be interpreted
+		wikitext = wikitext.replace (new RegExp("&", "g"), "&amp;"); //for entities
+		wikitext = wikitext.replace (new RegExp("<", "g"), "&lt;"); //for comments
+		return wikitext;
+	}
+	function wrapTemplate(wikixml)
+	{	
+		wikixml.find("data").each( function() {
+			var elem = $(this);
+			var txt = doNotInterpertSpecialCharacters(elem.text());
+			//nowiki can also be employed :?
+			txt = txt.replace (new RegExp("{{", "g"), "<template>{{");
+			txt = txt.replace (new RegExp("}}", "g"), "}}</template>");
+			elem.html(txt);
+		});
+		wikixml.find("data").contents().filter(function(){ return this.nodeType == 3; }).wrap("<notemplate/>");
+		return wikixml;
+	}
+	
+	window.WikiXml = {};
+	
+	window.WikiXml.defaultOptions = {
+		"wrapCategory": false,
+		"wrapSplitter": false,
+		"wrapTemplate": false,
+		"wrapInterwikis": false
+	};
+	
+	window.WikiXml.parseWikitext = function(wikitext, options)
+	{
+		options = options || {};
+		for (var opt in window.WikiXml.defaultOptions)
 		{
-			wikixml += "</data>";
-			var curLevel = (line.split ("=").length - 1) / 2;
-			if (oldLevel + 2 <= curLevel)
-				return alert("something wrong in wikitext");
- 
-			if (oldLevel >= curLevel) {
-				wikixml += Array (oldLevel - curLevel + 2).join("</section>");
+			if (options[opt] === undefined) options[opt] = window.WikiXml.defaultOptions[opt];
+		}
+		
+		wikitext = doNotInterpertSpecialCharacters(wikitext);
+		
+		var wikilist = wikitext.split("\n");
+		var upto = wikilist.length - 1;
+		if (wikilist[upto] === "")
+			wikilist.pop(), upto--; //last char is always a newline in editting mode
+		
+		var interwikis = "";
+		if (options.wrapInterwikis)
+		{
+			var line = wikilist[upto];
+			while(/\[\[...?:.+\]\]/.test(line) && upto > 0)
+			{
+				interwikis = line + "\n" + interwikis;
+				upto--;
+				line = wikilist[upto];
 			}
-			oldLevel = curLevel;
-			wikixml += "<section class = '" + line.replace(/=/g, "") + "' level = '" + oldLevel + "'>";
-			wikixml += "<data>";
 		}
-		else
+		
+		let oldLevel = 1;
+		
+		let wikixml = "<xml><data>";
+		for (let index = 0; index <= upto; index++)
 		{
-			wikixml += line;
+			let line = wikilist[index];
+			if (line[0] == '=' && line[line.length - 1] == '=')
+			{
+				wikixml += "</data>";
+				let curLevel = (line.split ("=").length - 1) / 2;
+				if (curLevel >= oldLevel + 2 && oldLevel != 1)
+				{
+					curLevel = oldLevel + 1;
+					//myalert("something wrong in wikitext:\ncurLevel: " + curLevel + ", oldlevel: " + oldLevel + "\nline: " + line);
+					//return undefined;
+				}
+	
+				if (oldLevel >= curLevel) {
+					wikixml += Array (oldLevel - curLevel + 2).join("</section>");
+				}
+				oldLevel = curLevel;
+				let sectionName = line.replace(/=/g, "");
+				if (oldLevel == 2) //language sections should have underscores for languages like Old_Georgian.
+					sectionName = sectionName.replace(/ /g, "_");
+				
+				wikixml += "<section class = '" + sectionName + "' level = '" + oldLevel + "'>";
+				wikixml += "<data>";
+			}
+			else
+			{
+				let pre = "", post = "";
+				if (line == "----" && options.wrapSplitter)
+				{
+					pre = "</data><splitter>";
+					post = "</splitter><data>";
+				}
+				if (line.startsWith("[[Category:") && options.wrapCategory)
+				{
+					pre = "</data><category>";
+					post = "</category><data>";
+				}
+				
+				wikixml += pre + line + post;
+			}
+			wikixml += "\n";
 		}
-		wikixml += "\n";
-	}
- 
-	//close sections. 
-	wikixml += "</data>";
-	var curLevel = 2;
-	if (oldLevel >= curLevel) {
-		wikixml += Array (oldLevel - curLevel + 2).join("</section>");
-	}			
-	wikixml += "</xml>";
-	wikixml = $($.parseXML(wikixml));
-	return wikixml;
-}
- 
- 
- 
-function deXMLize(wikixml)
-{
-	wikixml.find("section").map(function(){ 
-		var This = $(this);
-		var header = This.attr("class");
-		var level = parseInt(This.attr("level"));
-		level = Array(level + 1).join("=");
-		This.prepend(level + header + level);
-	});
-	return wikixml.text().slice(0, -1);
-}
- 
-//START of OPERATIONS ON XML
-function splitByTemplateUse(wikixml)
-{	
-	wikixml.find("data").each( function() {
-		var elem = $(this);
-		var txt = doNotInterpertSpecialCharacters(elem.text());
-		//nowiki can also be employed :?
-		txt = txt.replace (new RegExp("{{", "g"), "<templateuse>{{");
-		txt = txt.replace (new RegExp("}}", "g"), "}}</templateuse>");
-		elem.html(txt);
-	});
-	wikixml.find("data").contents().filter(function(){ return this.nodeType == 3; }).wrap("<notemplate/>")
-	return wikixml;
-}
-//END of OPERATIONS ON XML
- 
- 
- 
-if (mw.config.values.wgPageName == "User:Dixtosa/XMLize.js/test" && mw.config.values.wgAction == "edit")
-{
-	try
-	{
-		var wpTextbox1 = $("#wpTextbox1");
-		var new_wpTexbox1 = deXMLize(XMLize(wpTextbox1.text()));
-		if (new_wpTexbox1 != wpTextbox1.text())
+		
+		//close sections. 
+		wikixml += "</data>";
+		let curLevel = 2;
+		if (oldLevel >= curLevel) {
+			wikixml += Array (oldLevel - curLevel + 2).join("</section>");
+		}
+		
+		wikixml += "<interwikis>" + interwikis + "</interwikis>";
+		
+		wikixml += "</xml>";
+		wikixml = $($.parseXML(wikixml));
+		
+		if (options.wrapTemplate)
 		{
-			console.log(new_wpTexbox1);
-			throw "mismatch between original and deXMLized XMLized original wikitext\n";
+			wikixml = wrapTemplate(wikixml);
 		}
-		alert("XMLization works!");
-	}
-	catch(e)
+		return wikixml;
+	};
+	
+	
+	
+	window.WikiXml.toWikitext = function(wikixml)
 	{
-		alert (e);
+		wikixml.find("section").map(function(){ 
+			var This = $(this);
+			var header = This.attr("class");
+			var level = parseInt(This.attr("level"));
+			if (level == 2)
+				header = header.replace(/_/g, " ");
+				
+			level = Array(level + 1).join("=");
+			This.prepend(level + header + level);
+		});
+		return wikixml.text(); //commenting out as I do not remember what it did - .slice(0, -1);
+	};
+	
+	
+	//sanity check
+	window.WikiXml.test = function(options)
+	{
+		try
+		{
+			var wpTextbox1 = $("#wpTextbox1");
+			var new_wpTexbox1 = WikiXml.toWikitext(WikiXml.parseWikitext(wpTextbox1.text(), options));
+			if (new_wpTexbox1 != wpTextbox1.text())
+			{
+				console.log(new_wpTexbox1);
+				throw "mismatch between original and deXMLized XMLized original wikitext\n";
+			}
+			myalert("test #N: XMLization works!");
+		}
+		catch(e)
+		{
+			myalert (e);
+		}
+	};
+	if (mw.config.values.wgPageName == "User:Dixtosa/XMLize.js/test" && mw.config.values.wgAction == "edit")
+	{
+		window.WikiXml.test({});
+		window.WikiXml.test({wrapInterwikis});
+		window.WikiXml.test({wrapCategory});
 	}
 }
