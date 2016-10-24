64e9899aadbbb04e3aa7128368ffe90c0d33e156
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2016-10-24 23:18:14 +0400

    Update highlightme.js
    ES6ize

--- User Scripts/highlightme.js
+++ User Scripts/highlightme.js
@@ -1,14 +1,15 @@
-// Description         : this is a very little script, but does one thing well.
+// Description         : this is a very little script and does a very little thing, but does it well.
 // Real description xD : highlights your username in history (shows your contribution)
 // Problems/to-do      : maybe a button somewhere "show my contrib" instead of highlighting
 // Dependency          : None
 // UserPrefs           : None
- 
-function highlight(uname)
-{
-	var me = $ (".mw-userlink").filter(function(){ return $(this).text() == uname; });
-	me.css({"color" : "black", "font-weight" : "bold"});
-}
- 
+
+
+
 if (mw.config.values.wgAction == "history")
-    highlight(mw.config.values.wgUserName);
+{
+	$( () => {
+		var username = mw.config.values.wgUserName;
+		$(".mw-userlink").filter((i, elem) => elem.text == username).css({"color" : "black", "font-weight" : "bold"});
+	});
+}
