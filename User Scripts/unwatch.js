4b121544cef9cac600aca0a28fca9ecbee2b3d74
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2016-10-24 23:22:00 +0400

    Update unwatch.js
    ES6ized

--- User Scripts/unwatch.js
+++ User Scripts/unwatch.js
@@ -1,47 +1,37 @@
 // Description         : this is a very small script that does one thing, but does it well. Unix FTW. 
-// Real description xD : adds unwatch button between "diff" and "hist"
-// Idea by             : after [[User:Hippietrail]]'s version stopped working I reimplemented it.
-// Problems/to-do      : The response of AJAX request says title is deprecated :(
+// Real description    : adds unwatch button between "diff" and "hist"
+// Idea by             : when [[User:Hippietrail]]'s version stopped working I reimplemented it.
+// Problems/to-do      : None
 // Dependency          : None
 // UserPrefs           : None 
 
 function unwatch(Title)
 {
-	Title = decodeURIComponent(Title);
-	$.ajax({
-		url: mw.util.wikiScript( 'api' ),
-		type: 'POST',
-		data: {
-			format: 'json',
-			action: 'watch', //or unwatch :?
-			title: Title,
-			unwatch: 1,
-			uselang: "en", 
-			token: mw.user.tokens.get( 'watchToken' )
-		}
-	})
-	.done (function( data ) {
-		if ( data && data.watch && data.watch.message && data.watch.message.search ("has been removed from") > 0) {
-			console.log( 'Page edited!' );
- 
-			//remove from watchlist
-			$(".mw-changeslist li .mw-title:contains('" + Title.replace("'", "\\'") + "')").parent().hide("slow");
+	let title = decodeURIComponent(Title);
+	new mw.Api().unwatch(title).done (function( data ) {
+		if ( data && data.message.search ("been removed from") > 0) {
+			$(`.mw-changeslist li .mw-title:contains('${title.replace("'", "\\'")}')`).parent().hide("slow");
 		} else {
-			alert( 'The edit query returned an error.' );
+			mw.notify( 'The edit query returned an error.' + JSON.stringify(data) );
 		}
 	})
 	.fail ( function() {
-		alert( 'The ajax request failed.' );
+		mw.notify( 'The ajax request failed.' );
 	});
 }
  
 function addUnwatch()
 {
-	var arr = $ ("a:contains('diff')"); //probably a bit sloppy
-	arr.after(function(i) {
+	$("a:contains('diff')").after(function(index) {
 		//I'm vegetarian
-		return " | <a href='javascript: unwatch(\"" + arr[i].title.replace("'", "%27") + "\")'>unwatch</a>";
+		var escapedTitle = this.title.replace("'", "%27");
+		return ` | <a href='javascript: unwatch("${escapedTitle}")'>unwatch</a>`;
 	});
 }
 
-if( wgCanonicalSpecialPageName == 'Watchlist' && wgAction == 'view' ) $(addUnwatch);
+if (mw.config.values.wgCanonicalSpecialPageName == 'Watchlist' && mw.config.values.wgAction == 'view' ) 
+{
+	mw.loader.using(['mediawiki.api.watch'], function () {
+		$(addUnwatch);
+	});
+}
