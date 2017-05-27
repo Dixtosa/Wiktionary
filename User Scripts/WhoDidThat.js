70901ffaebc0fde0fc41a8c2e111ab7fa2e66add
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2017-05-27 15:43:38 +0400

    Update WhoDidThat.js

--- User Scripts/WhoDidThat.js
+++ User Scripts/WhoDidThat.js
@@ -1,8 +1,7 @@
 //Description: A new button in the toolbar helps to trace the user who caused the revision including the selected text
-//To-do: expand templates
+//To-do: expand templates?
 //To-do: use binary-search instead of linear probing
-//To-do: implement continuation
-
+//To-do: problems with deleted revisions
 
 //working urls:
 //https://en.wiktionary.org/w/api.php?titles=???????&action=query&prop=revisions&rvlimit=max
@@ -10,84 +9,73 @@
 //https://en.wiktionary.org/w/api.php?titles=???????&action=query&prop=revisions&rvlimit=max&format=json
 //https://www.mediawiki.org/wiki/API:Revisions
 
-var text;
-var page = mw.config.values.wgPageName;
 
-function get_revision(revid)
-{
-	var ans = "";
-	$.ajax({
-			'url' : mw.util.wikiScript( 'index' ),
-			'data': {
-					'action' : 'raw',
-					'title' : page,
-					'oldid' : revid
-			},
-			'type': 'GET',
-			'success': function( data ) { ans = data; } ,
-			'error ': function( xhr ) { alert( 'Error    Request failed.' );} ,
-			async:false
-	});
-	return ans;
+if (mw.config.values.wgAction == "view"){
+	mw.loader.using( ['mediawiki.util'] ).then( function () {
+		mw.util.addPortletLink( 'p-tb', 'javascript:WhoDidThat();', 'Who did that? (blame)', 'WhoDidThatJsBtn', "Find out who introduced selected text?", "");
+	});	
 }
 
 function WhoDidThat()
 {
-	text = window.getSelection().toString();
-	getHistory();
-}
+	let text = window.getSelection().toString();
+	if (text === "") mw.notify("Nothing is selected on the page!");
+	else goToRevision(text);
 
-function getHistory()
-{
-	$.ajax({
-			'url' : mw.util.wikiScript( 'api' ),
-			'data' : {
-				'titles' : page,
+	
+	mw.loader.load("mediawiki.notify");
+	//inner functions
+	function search_algorithm(text, revisions)
+	{
+		if (revisions.length <= 1) return 0; 
+		
+		for (var i = 1; i < revisions.length; i++)
+		{
+			if (revisions[i].texthidden !== undefined) continue;
+			if (revisions[i]["*"].search(text) == -1) return revisions[i - 1].revid;
+		}
+		return -1;
+	}
+	
+	function goToRevision(text, pContinueData)
+	{
+	
+		mw.notify("The script may take some time...");
+		
+		var params = {
+				'titles' : mw.config.values.wgPageName,
 				'action' : 'query',
 				'prop' : 'revisions',
 				'rvlimit' : 'max',
+				'rvprop': 'ids|content', //'ids|flags|timestamp|comment|user|content',
 				'format' : 'json'
-			},
-			'type' : 'GET',
-			'success': function( data ) {
-					var pageid = Object.keys(data.query.pages)[0];
-					var revisions = data.query.pages[pageid].revisions;
-					var revids = revisions.map(function(rev){ return rev.revid; });
-					var ans = algorithm (text, revids);
+		};
+		if (pContinueData && pContinueData.rvcontinue)
+			params.rvcontinue = pContinueData.rvcontinue;
+		
+		$.get(mw.util.wikiScript( 'api' ), params, function( data ) {
+					var continueData = data.continue;
+					var articleId = mw.config.values.wgArticleId;
+					var revisions = data.query.pages[articleId].revisions;
+					var ans = search_algorithm(text, revisions);
+					if (ans === -1 && continueData)
+					{
+						return goToRevision(text, continueData);
+					}
 					switch(ans)
 					{
 						case 0:
 							alert("revisions.length <= 1");
 							break;
 						case -1:
-							alert("from the begginng");
+							alert("from the beginning");
 							break;
 						default:
-							//alert("revision number: " + ans);
-							location = "https://en.wiktionary.org/w/index.php?title=" + page + "&diff=" + ans;
+							location.href = "/w/index.php?title=" + articleId + "&diff=" + ans;
 							break;
 					}
-			},
-			'error ': function( xhr ) {
+			}).fail(function( xhr ) {
 					alert( 'Error    Request failed.' );
-			}
-	});
-	alert("It will take some time...");
-	alert("Really.");
-}
-
-function algorithm(text, revisions)
-{
-	if (revisions.length <= 1) return 0; 
-	
-	for (var i = 1; i < revisions.length; i++)
-	{
-		//i - 1 contains 
-		//check if i contains too
-		if (get_revision(revisions[i]).search(text) == -1) return revisions[i - 1];
+			});
 	}
-	return -1;
 }
-
-if (mw.config.values.wgAction == "view")
-	mw.util.addPortletLink( 'p-tb', 'javascript:WhoDidThat();', 'Who Did that?', 'id?', "", "");
