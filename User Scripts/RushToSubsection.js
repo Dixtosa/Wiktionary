41550e45b29d57b7069ab6b187943d77310d9ca6
Giorgi Eufshi <Dixtosa@users.noreply.github.com>
2019-01-09 23:54:26 +0400

    Delete RushToSubsection.js

--- User Scripts/RushToSubsection.js
+++
@@ -1,8 +0,0 @@
-if (['Watchlist', 'Recentchanges', 'Recentchangeslinked'].indexOf(mw.config.values.wgCanonicalSpecialPageName) > -1
-	&& mw.config.values.wgAction == 'view' ) 
-$(() => {
-	$(".mw-changeslist ul li .mw-title a").attr("href", function() {
-		let rightArrowLink = $(this).parent().parent().find(".autocomment").parent().prev();
-		if (rightArrowLink.length == 1) return rightArrowLink.attr("href");
-	});
-});
